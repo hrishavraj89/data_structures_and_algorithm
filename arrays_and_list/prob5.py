3a71ca79c28dab103f7748e07036e880a2410aec
hrishavraj89 <hrishabraj89@gmail.com>
2025-09-06 21:51:33 +0530

    solved prob 5

---
+++ arrays_and_list/prob5.py
@@ -0,0 +1,9 @@
+#Write a script to create a list of first n terms of fibonacci series
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+    
+for i in range(10):
+    print(fib(i), end = ' ')
